cb285253af015756c8663bcac7f1c1c37b5d17df
Amy Shackles <amyshackles@gmail.com>
2020-09-28 23:43:43 -0700

    Fixes the accidental inclusion of \r

--- server/extractCountryData.js
+++ server/extractCountryData.js
@@ -1,5 +1,5 @@
 module.exports = function(csvString) {
-    let csvArray = csvString.split("\n");
+    let csvArray = csvString.split('\n');
 
     // Remove the column names from csvArray
     let headings = csvArray.shift().split(',');
@@ -8,15 +8,15 @@ module.exports = function(csvString) {
     csvArray.forEach(function(csvRowString) {
         // Split each row by comma
         var csvRow = csvRowString.split(",");
-        let name = csvRow[0];
+        let name = csvRow[0].replace('\r', '');
         // Turn each row into an object with headings as keys
         let rowData = {}
         csvRow.forEach((row, index) => {
-            let heading = headings[index];
+            let heading = headings[index].replace('\r', '');
             if (rowData[name] === undefined) {
-                rowData[name] = { [heading]: row }
+                rowData[name] = { [heading]: row.replace('\r', '') }
             } else {
-                rowData[name][heading] = row;
+                rowData[name][heading] = row.replace('\r', '');
             }
         })
         countries[name] = Object.values(rowData)[0];
